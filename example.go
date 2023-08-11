cc7eb8d80425e3cf1275e4735a2609ec23cae42c
JackBekket <jack_out_the_b0x@proton.me>
2023-08-12 00:01:55 +0400

    handlers

--- example.go
+++ example.go
@@ -12,13 +12,13 @@ import (
 func main() {
 
 
-	_ = godotenv.Load()
-	board_password := os.Getenv("PASSWORD")
+	_ = godotenv.Load()	// load .env file locally
+	board_password := os.Getenv("PASSWORD") // get value from .env
 	b_endpoint := os.Getenv("ENDPOINT")
 	
 	// test
 	println("endpoint: ",b_endpoint)
-	println("password: ",board_password)
+	//println("password: ",board_password)
 
 
 	//client := jschan.NewClient("http://dev-jschan.lan")
@@ -44,11 +44,20 @@ func main() {
 		}
 	}
 
-	/*
+	GetBanners(*client,ctx)
+	GetBoards(*client,ctx,"b")
+	GetBoardIndexPages(*client,ctx,"b")
+
+	GetRecentPosts(*client,ctx,"b")
+
+
+}
+
+func GetReports(ip string, board_name string,client jschan.Client, ctx context.Context) {
 	manageReportsOptions := &jschan.GetManageReportsOptions{
 		Page:  0,
-		IP:    "10.0.0.192",
-		Board: "",
+		IP:    ip,
+		Board: board_name,
 	}
 	reports, err2 := client.GetManageReports(ctx, manageReportsOptions)
 	if err2 != nil {
@@ -57,6 +66,97 @@ func main() {
 	}
 	fmt.Printf("Fetched %d reports\n", len(reports.Reports))
 
-	*/
+}
+
+
+// stuff only
+func GetRecentPosts(client jschan.Client,  ctx context.Context, board_name string) {
+
+	getRecentOptions := &jschan.GetManageRecentOptions{
+		Board: board_name,
+	}
+
+	recent, err := client.GetManageRecent(ctx, getRecentOptions)
+	if err != nil {
+		fmt.Println(err)
+		return
+	  }
+	  fmt.Printf("%+v", recent)
+
 
 }
+
+
+// Get /b/
+func GetBoards(client jschan.Client,  ctx context.Context, search_request string) {
+
+	getBoardsPublicOptions := &jschan.GetBoardsPublicOptions{
+		Search:    search_request,
+	  }
+	  boards, err := client.GetBoardsPublic(ctx, getBoardsPublicOptions)
+	  if err != nil {
+		fmt.Println(err)
+		return
+	  }
+	  fmt.Printf("%+v", boards)
+}
+
+
+func GetBoardIndexPages(client jschan.Client, ctx context.Context, board_name string) {
+
+
+	getIndexOptions := &jschan.GetIndexOptions{
+		Board: board_name,
+		Page:  1,
+	  }
+	  index, err := client.GetIndex(ctx, getIndexOptions)
+	  if err != nil {
+		fmt.Println(err)
+		return
+	  }
+	  fmt.Printf("%+v", index)
+}
+
+func GetAllThreadsBoard(client jschan.Client, ctx context.Context, board_name string) {
+
+	getCatalogOptions := &jschan.GetCatalogOptions{
+		Board: board_name,
+	  }
+	  catalog, err := client.GetCatalog(ctx, getCatalogOptions)
+	  if err != nil {
+		fmt.Println(err)
+		return
+	  }
+	  fmt.Printf("%+v", catalog)
+}
+
+
+func GetCustomPage(client jschan.Client, ctx context.Context,board_name string,page_name string) {
+
+	getCustomPageOptions := &jschan.GetCustomPageOptions{
+		Board: board_name,
+		CustomPage: page_name,
+	  }
+	  custompage, err := client.GetCustomPage(ctx, getCustomPageOptions)
+	  if err != nil {
+		fmt.Println(err)
+		return
+	  }
+	  fmt.Printf("%+v", custompage)
+}
+
+
+func GetBanners(client jschan.Client, ctx context.Context) {
+
+	getBannersOptions := &jschan.GetBannersOptions{
+		Board: "t",
+	  }
+	  banners, err := client.GetBanners(ctx, getBannersOptions)
+	  if err != nil {
+		fmt.Println(err)
+		return
+	  }
+	  fmt.Printf("%+v", banners)
+}
+
+
